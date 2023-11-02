236011d7b532d81316a935e6855dbabaa0c8246f
Samrendra <samrendrakumar893@gmail.com>
2023-11-02 21:32:22 +0530

    Setup of smtp using node mailer

---
+++ src/config/emailConfig.js
@@ -0,0 +1,10 @@
+const nodemailer= require('nodemailer');
+const {EMAIL_ID,EMAIL_PASSWORD} = require('./serverConfig')
+const sender = nodemailer.createTransport({
+    service:'Gmail' ,
+    auth:{
+        user:EMAIL_ID,
+        pass:EMAIL_PASSWORD
+    }
+})
+module.exports = sender 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 const express = require('express') ;
 const bodyParser = require('body-parser');
 const {PORT} = require('./config/serverConfig');
+const {sendBasicEmail} = require('./services/email-service')
     const setupAndStartServer = () => {
     const app= express() ;
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended:true }))
     app.listen(PORT,()=>{
     console.log(`Server started at port no ${PORT}`)
+    sendBasicEmail(
+        'support@admin.com',
+        'kumarsamrendra24@gmail.com',
+        'This is a testing email',
+        'Hey, how are you,I hope you like the support'
+    )
     })
 }
 setupAndStartServer() ;
